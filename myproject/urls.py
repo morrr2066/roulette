0ae4ac438da7c930994e8a56ff672ae37a7c3975
Marwan Essam <marwanessam368@gmail.com>
2025-05-21 21:44:46 +0300

    Add home view route and fix URLs

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('roulette/', include('roulette.urls', namespace='roulette')),
+    path('', views.home, name='home'),
 ]
